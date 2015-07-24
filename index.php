af132715c2800056ad6c857d04199cbc2b57bc54
Alfred <alfredsin826@hotmail.com>
2015-07-24 16:51:19 -0700

    First commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+    <title>Restaurants</title>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
+    <link href="./custom.css" rel="stylesheet" />
+</head>
+<body>
+    <div id="map-canvas"></div>
+</body>
+<script type="text/javascript" src="./googlemaps.js"></script>
